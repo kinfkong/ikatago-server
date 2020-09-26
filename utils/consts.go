329132fc857b8711b61c13b346bee93e6a5fd802
kinfkong <kinfkong@126.com>
2020-09-26 11:53:25 +0800

    bump version

--- utils/consts.go
+++ utils/consts.go
@@ -2,7 +2,7 @@ package utils
 
 const (
 	// ServerVersion the server version
-	ServerVersion = "1.4.0"
+	ServerVersion = "1.4.1"
 	// WorldURL represents the root url of the default world
 	WorldURL = "https://ikatago-fairyland.oss-cn-beijing.aliyuncs.com/world.json"
 )
