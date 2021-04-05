303329f01fc798d1b2f3c3dc1fcc200bd4886710
kinfkong <kinfkong@126.com>
2021-04-05 09:26:49 +0800

    tests

--- utils/consts.go
+++ utils/consts.go
@@ -2,7 +2,7 @@ package utils
 
 const (
 	// ServerVersion the server version
-	ServerVersion = "1.5.2"
+	ServerVersion = "1.6.0"
 	// WorldURL represents the root url of the default world
 	WorldURL = "https://ikatago-fairyland.oss-cn-beijing.aliyuncs.com/world.json"
 )
